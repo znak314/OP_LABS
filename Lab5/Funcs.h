0203c4b2d810cd1b441ee05d94bfe17ce5db090d
znak314 <114077337+znak314@users.noreply.github.com>
2023-05-08 11:48:42 +0300

    Create Funcs.h

---
+++ Lab5/Funcs.h
@@ -0,0 +1,8 @@
+int EnterSize(string s);
+void PrintBinMatrix(TIntNumber2* A, int size);
+void PrintHexMatrix(TIntNumber16* A, int size);
+void BinIncrementation(TIntNumber2* A, int size);
+void HexDecrementation(TIntNumber16* A, int size);
+int find_biggest_bin(TIntNumber2* A, int size);
+int find_biggest_hex(TIntNumber16* A, int size);
+int FindBiggestNum(TIntNumber2* bin, int size1, TIntNumber16* hex, int size2);
