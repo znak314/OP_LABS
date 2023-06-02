663f0d54bef08058eeaf2e5623a95d23abd08edd
znak314 <114077337+znak314@users.noreply.github.com>
2023-06-02 10:49:29 +0300

    Create func.h

---
+++ lab7+8/func.h
@@ -0,0 +1,9 @@
+#include<cwctype>
+#include"Classes.h"
+
+bool is_leap(int year);
+int GenerateDay(int year, int month);
+bool isStringEmpty(const std::string& str);
+bool is_number(const std::string& str);
+bool is_Date_Exist(int year, int month, int day);
+int Compare( Date& A,  Date& B);
